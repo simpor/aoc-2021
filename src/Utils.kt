3d5510d037c6c1fc56bc2f72440e20ba92907743
Simon Rydberg <simpor81@gmail.com>
2021-11-30 21:03:54 +0100

    day 1 template

--- src/Utils.kt
+++ src/Utils.kt
@@ -1,6 +1,8 @@
 import java.io.File
 import java.math.BigInteger
 import java.security.MessageDigest
+import com.github.mm.coloredconsole.colored
+
 
 /**
  * Reads lines from the given input txt file.
@@ -11,3 +13,97 @@ fun readInput(name: String) = File("src", "$name.txt").readLines()
  * Converts string to md5 hash.
  */
 fun String.md5(): String = BigInteger(1, MessageDigest.getInstance("MD5").digest(toByteArray())).toString(16)
+
+data class Point(val x: Int, val y: Int)
+data class Point3(var x: Int, var y: Int, var z: Int)
+
+operator fun Point3.plus(other: Point3): Point3 {
+    return Point3(
+        this.x + other.x,
+        this.y + other.y,
+        this.z + other.z
+    )
+}
+
+object AoCUtils {
+
+    fun readText(source: String): String =
+        File(ClassLoader.getSystemResource(source).file).readText().replace("\r\n", "\n")
+
+    fun readLines(source: String): List<String> = readText(source).lines()
+
+
+    infix fun <T> T.test(pair: Pair<T, String>) =
+        if (this == pair.first)
+            colored {
+                println("PASS - ${this@test} is correct - ${pair.second}".cyan.bold)
+            }
+        else
+            colored {
+                println("FAIL - ${this@test} does not match ${pair.first} - ${pair.second}".red.bold)
+            }
+
+
+    fun <T> uniqueNumber(input: List<T>): List<List<T>> {
+        val returnList = mutableListOf<List<T>>()
+        val allCombos = allCombos(input, 5)
+        allCombos.forEach {
+            val str = it
+            var add = true
+            str.forEachIndexed { i1, c1 ->
+                str.forEachIndexed { i2, c2 ->
+                    if (i1 != i2 && c1 == c2) {
+                        add = false
+                    }
+                }
+            }
+            if (add) returnList.add(str)
+        }
+        return returnList
+    }
+
+    fun <T> allCombos(set: List<T>, k: Int): List<List<T>> {
+        val n = set.size
+        val list = mutableListOf<MutableList<T>>()
+        allCombosRec(set, mutableListOf(), n, k, list);
+        return list
+    }
+
+    fun <T> allCombosRec(
+        set: List<T>,
+        prefix: MutableList<T>,
+        n: Int, k: Int, list: MutableList<MutableList<T>>,
+    ) {
+        if (k == 0) {
+            list.add(prefix)
+            return
+        }
+        for (i in 0 until n) {
+            val newPrefix = prefix + set[i]
+            allCombosRec(set, newPrefix.toMutableList(), n, k - 1, list)
+        }
+    }
+}
+
+// Driver Code
+fun main() {
+    val set1 = listOf("a", "b")
+    val k = 3
+    println("First test " + AoCUtils.allCombos(set1, k))
+
+    val set2 = listOf("a", "b", "c", "d")
+    val k2 = 1
+    println("Second test " + AoCUtils.allCombos(set2, k2))
+
+    val set3 = listOf("0", "1", "2", "3", "4")
+    val k3 = 5
+    println("Third test: " + AoCUtils.allCombos(set3, k3))
+
+    println("Unique test: " + AoCUtils.uniqueNumber(set3))
+
+
+    listOf("", "").mapIndexed { index, s -> Pair(index, s) }
+        .toMap()
+
+
+}
--- src/template/template.kt
+++ src/template/template.kt
@@ -1,29 +1,26 @@
+package template
 import AoCUtils
 import AoCUtils.test
 
 //val input = AoCUtils.readText("")
 
 fun main() {
-    fun part1(input: String): Int {
-        return 0
-    }
-
-    fun part2(input: String): Int {
-        return 0
-    }
-
-
-    val testInput = ""
-    val input = ""
-
     part1(testInput) test Pair(0, "test 1 part 1 should be 0")
     part1(input) test Pair(0, "part 1 should be 0")
 
     part2(testInput) test Pair(0, "test 2 part 2 should be 0")
     part2(input) test Pair(0, "part 2 should be 0")
 
+}
 
+fun part1(input: String): Int {
+    return 0
+}
+
+fun part2(input: String): Int {
+    return 0
 }
 
 
-
+val testInput = ""
+val input = ""
