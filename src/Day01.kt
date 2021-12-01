0532299bd07ec52b5e43cb9df3a152b26bf2a8fb
Simon Rydberg <simpor81@gmail.com>
2021-12-01 06:49:43 +0100

    Day 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,22 +1,45 @@
-import AoCUtils
 import AoCUtils.test
+import java.io.File
 
 //val input = AoCUtils.readText("")
 
 fun main() {
     fun part1(input: String): Int {
-        return 0
+        val list = input.split("\n").map { s -> s.toInt() }
+        var counter = 0
+        for (i in 1 until list.size) {
+            if (list[i - 1] < list[i]) counter++
+        }
+
+        return counter
     }
 
     fun part2(input: String): Int {
-        return 0
+        val list = input.split("\n").map { s -> s.toInt() }
+        var counter = 0
+        for (i in 1 until (list.size - 2)) {
+            val w1 = list[i - 1] + list[i] + list[i + 1]
+            val w2 = list[i] + list[i + 1] + list[i + 2]
+            if (w1 < w2) counter++
+        }
+
+        return counter
     }
 
 
-    val testInput = ""
-    val input = ""
+    val testInput = "199\n" +
+            "200\n" +
+            "208\n" +
+            "210\n" +
+            "200\n" +
+            "207\n" +
+            "240\n" +
+            "269\n" +
+            "260\n" +
+            "263"
+    val input = File("src", "Day01.txt").readText()
 
-    part1(testInput) test Pair(0, "test 1 part 1 should be 0")
+    part1(testInput) test Pair(7, "test 1 part 1 should be 0")
     part1(input) test Pair(0, "part 1 should be 0")
 
     part2(testInput) test Pair(0, "test 2 part 2 should be 0")
