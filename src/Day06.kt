19033bdf06eef54ef82d40f99659cc834c66c6cd
Simon Rydberg <simpor81@gmail.com>
2021-12-06 06:32:38 +0100

    Day 6

---
+++ src/Day06.kt
@@ -0,0 +1,60 @@
+import AoCUtils.test
+import java.io.File
+
+fun main() {
+    data class Fish(var days: Int, var count: Long = 1)
+
+    fun part1(input: String): Long {
+        var fishes = input.split(",").map { it.trim().toInt() }.map { Fish(it) }.toMutableList()
+        for (day in 1..80) {
+            var newFishes = 0L
+            fishes.forEach { fish ->
+                fish.days = fish.days - 1
+                if (fish.days == -1) {
+                    newFishes += fish.count
+                    fish.days = 6
+                }
+            }
+
+            if (newFishes != 0L) fishes.add(Fish(8, newFishes))
+        }
+
+
+        return fishes.map { it.count }.sum()
+    }
+
+    fun part2(input: String): Long {
+        var fishes = input.split(",").map { it.trim().toInt() }.map { Fish(it) }.toMutableList()
+        for (day in 1..256) {
+            var newFishes = 0L
+            fishes.forEach { fish ->
+                fish.days = fish.days - 1
+                if (fish.days == -1) {
+                    newFishes += fish.count
+                    fish.days = 6
+                }
+            }
+
+            if (newFishes != 0L) fishes.add(Fish(8, newFishes))
+        }
+
+
+        return fishes.map { it.count }.sum()
+    }
+
+
+    val testInput = "3,4,3,1,2"
+
+    val input = File("src", "Day06.txt").readText()
+
+    part1(testInput) test Pair(5934, "test 1 part 1")
+    part1(input) test Pair(362666, "part 1")
+
+    part2(testInput) test Pair(26984457539, "test 2 part 2")
+    part2(input) test Pair(1640526601595, "part 2")
+
+
+}
+
+
+
