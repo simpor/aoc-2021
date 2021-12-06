c27b962a8950ef8fd930665f21110bb9eb0826af
Simon Rydberg <simpor81@gmail.com>
2021-12-06 06:39:12 +0100

    Day 6 cleanup

--- src/Day06.kt
+++ src/Day06.kt
@@ -5,18 +5,18 @@ fun main() {
     data class Fish(var days: Int, var count: Long = 1)
 
     fun part1(input: String): Long {
-        var fishes = input.split(",").map { it.trim().toInt() }.map { Fish(it) }.toMutableList()
+        val fishes = input.split(",").map { it.trim().toInt() }.map { Fish(it) }.toMutableList()
         for (day in 1..80) {
-            var newFishes = 0L
+            var newborn = 0L
             fishes.forEach { fish ->
                 fish.days = fish.days - 1
                 if (fish.days == -1) {
-                    newFishes += fish.count
+                    newborn += fish.count
                     fish.days = 6
                 }
             }
 
-            if (newFishes != 0L) fishes.add(Fish(8, newFishes))
+            if (newborn != 0L) fishes.add(Fish(8, newborn))
         }
 
 
@@ -24,18 +24,18 @@ fun main() {
     }
 
     fun part2(input: String): Long {
-        var fishes = input.split(",").map { it.trim().toInt() }.map { Fish(it) }.toMutableList()
+        val fishes = input.split(",").map { it.trim().toInt() }.map { Fish(it) }.toMutableList()
         for (day in 1..256) {
-            var newFishes = 0L
+            var newborn = 0L
             fishes.forEach { fish ->
                 fish.days = fish.days - 1
                 if (fish.days == -1) {
-                    newFishes += fish.count
+                    newborn += fish.count
                     fish.days = 6
                 }
             }
 
-            if (newFishes != 0L) fishes.add(Fish(8, newFishes))
+            if (newborn != 0L) fishes.add(Fish(8, newborn))
         }
 
 
