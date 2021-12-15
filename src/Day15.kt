837b1aa0e5828f93f0961ba8b81879804b629eed
Simon Rydberg <simpor81@gmail.com>
2021-12-15 22:48:54 +0100

    Day 15

--- src/Day15.kt
+++ src/Day15.kt
@@ -1,5 +1,6 @@
 import AoCUtils.test
 import java.io.File
+import java.util.*
 
 fun main() {
 
@@ -40,99 +41,73 @@ fun main() {
         return costMap[goal]!!
     }
 
+    fun Point.neighbours() = listOf(
+        Point(x, y - 1),
+        Point(x, y + 1),
+        Point(x + 1, y),
+        Point(x - 1, y)
+    )
+
     fun part2(input: String, debug: Boolean = false): Long {
+        data class Node(val x: Int, val y: Int, val cost: Long = 0) {
+            var sum: Long? = null
+
+            fun neighbours(completeMap: MutableMap<Point, Node>) = Point(x, y).neighbours().mapNotNull { completeMap[it] }
+        }
+
         val map = input.lines()
-            .mapIndexed { y, row -> row.toList().mapIndexed { x, v -> Point(x, y) to v.toString().toLong() } }.flatten()
+            .mapIndexed { y, row ->
+                row.toList().mapIndexed { x, v -> Point(x, y) to Node(x, y, v.toString().toLong()) }
+            }.flatten()
             .toMap()
         val mapSize = map.keys.maxByOrNull { it.x + it.y }!!
 
-        val completeMap = mutableMapOf<Point, Long>()
-        for (y in 0..9) {
-            println()
-            for (x in 0..9) {
-                print(map[Point(x, y)])
-            }
-        }
-        println()
-
-        // increase input
+        val completeMap = mutableMapOf<Point, Node>()
         // y....
         for (x in 0..mapSize.x) {
-            for (y in 0..((mapSize.y + 1) * 5)) {
+            for (y in 0 until ((mapSize.y + 1) * 5)) {
                 if (y <= mapSize.y) {
                     completeMap[Point(x, y)] = map[Point(x, y)]!!
                 } else {
                     val prevY = y - mapSize.y - 1
                     val prev = Point(x, prevY)
                     val current = Point(x, y)
-                    val cost = if (completeMap[prev]!! == 9L) 1L else completeMap[prev]!! + 1
-                    completeMap[current] = cost
+                    val cost = if (completeMap[prev]!!.cost == 9L) 1L else completeMap[prev]!!.cost + 1
+                    completeMap[current] = Node(x, y, cost)
                 }
-
             }
         }
-
-        for (y in 0..49) {
-            println()
-            for (x in 0..9) {
-                print(completeMap[Point(x, y)])
-            }
-        }
-        println()
 
         // x...
-        for (y in 0..((mapSize.y + 1) * 5)) {
-            for (x in (mapSize.x + 1)..((mapSize.x + 1) * 5)) {
+        for (y in 0 until ((mapSize.y + 1) * 5)) {
+            for (x in (mapSize.x + 1) until ((mapSize.x + 1) * 5)) {
                 val prevX = x - mapSize.x - 1
                 val prev = Point(prevX, y)
                 val current = Point(x, y)
-                val cost = if (completeMap[prev]!! == 9L) 1L else completeMap[prev]!! + 1
-                completeMap[current] = cost
+                val cost = if (completeMap[prev]!!.cost == 9L) 1L else completeMap[prev]!!.cost + 1
+                completeMap[current] = Node(x, y, cost)
             }
         }
 
-
-        for (y in 0..49) {
-            println()
-            for (x in 0..49) {
-                print(completeMap[Point(x, y)])
-            }
-        }
-        println()
-
-        val start = Point(0, 0)
-        val goal = completeMap.keys.maxByOrNull { it.x + it.y }!!
+        val start = Node(0, 0)
+        start.sum = 0
+        val goalPoint = completeMap.keys.maxByOrNull { it.x + it.y }!!
+        val goal = completeMap[goalPoint]!!
         println("start: $start, goal: $goal")
 
-
-        val costMap = mutableMapOf<Point, Long>()
-
-        for (y in 0..goal.y) {
-            println()
-            for (x in 0..goal.x) {
-//                print(completeMap[Point(x, y)])
-                val current = Point(x, y)
-                val up = Point(x, y - 1)
-                val left = Point(x - 1, y)
-
-                val upCost = costMap[up]
-                val leftCost = costMap[left]
-
-                if (upCost == null && leftCost == null) {
-                    costMap[current] = 0
-                } else if (upCost == null && leftCost != null) {
-                    costMap[current] = leftCost + completeMap[current]!!
-                } else if (upCost != null && leftCost == null) {
-                    costMap[current] = upCost + completeMap[current]!!
-                } else {
-                    val cheapest = if (leftCost!! < upCost!!) leftCost else upCost
-                    costMap[current] = cheapest + completeMap[current]!!
+        val visiting = PriorityQueue(compareBy(Node::sum))
+        visiting.add(start)
+        while (true) {
+            val node = visiting.poll()
+            if (node === goal)
+                return goal.sum!!
+            for (neighbour in node.neighbours(completeMap)) {
+                if (neighbour.sum == null) {
+                    neighbour.sum = neighbour.cost + node.sum!!
+                    visiting.offer(neighbour)
                 }
             }
         }
-        println()
-
-        return costMap[goal]!!
     }
 
 
@@ -148,13 +123,19 @@ fun main() {
                 "1293138521\n" +
                 "2311944581"
 
+    val test2 = "199111\n" +
+            "199191\n" +
+            "111191\n" +
+            "999991"
+
     val input = File("src", "Day15.txt").readText()
 
     part1(testInput) test Pair(40L, "test 1 part 1")
     part1(input) test Pair(673L, "part 1")
 
     part2(testInput) test Pair(315L, "test 2 part 2")
-//    part2(input) test Pair(0L, "part 2")
+    part2(test2) test Pair(12L, "test 2 part 2")
+    part2(input) test Pair(2893L, "part 2")  // to high: 2908
 
 
 }
