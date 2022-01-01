b29556ecc6c0d6a9dc1d851936f801ae332f31a6
Simon Rydberg <simpor81@gmail.com>
2022-01-01 17:45:49 +0100

    Added tests for Day 16

--- src/main/kotlin/year2021/Day16.kt
+++ src/main/kotlin/year2021/Day16.kt
@@ -2,7 +2,6 @@ package year2021
 
 import AoCUtils
 import AoCUtils.test
-import java.util.*
 
 class Day16 {
 
@@ -11,16 +10,15 @@ class Day16 {
     }
 
     private data class Operator(
-        val type: Int,
+        val typeId: Int,
         val version: Int,
         var operators: List<Operator> = listOf(),
         val values: MutableList<Long> = mutableListOf(),
-        var count: Int = 0,
+        var versions: List<Int> = listOf(version),
         var lengthType: Int = -1,
         var lengthTypeValue: Int = -1,
         var length: Int = -1,
         var binaryString: String = "",
-        val id: String = UUID.randomUUID().toString(),
     )
 
     private fun parseString(input: String): Pair<String, List<Operator>> {
@@ -34,7 +32,7 @@ class Day16 {
 
             val packetVersion = header.take(3).toInt(radix = 2)
             val packetId = header.drop(3).take(3).toInt(radix = 2)
-            val operator = Operator(packetId, packetVersion)
+            val operator = Operator(typeId = packetId, version = packetVersion)
             operators.add(operator)
 
             if (packetId == 4) {
@@ -64,8 +62,6 @@ class Day16 {
                     val numberOfSubPackages = binaryString.take(11).toInt(radix = 2)
                     operator.lengthTypeValue = numberOfSubPackages
                     binaryString = binaryString.drop(11)
-
-
                     val subOperators = (1..numberOfSubPackages).map { _ ->
                         val returnVal = parseString(binaryString)
                         binaryString = returnVal.first
@@ -74,7 +70,7 @@ class Day16 {
 
                     val returnVal = parseString(binaryString)
                     operator.operators = operator.operators + subOperators
-                    operator.count = operator.count + returnVal.second.size
+                    operator.versions = operator.versions + subOperators.map { it.versions }.flatten()
                     operator.binaryString = header + binaryString
                     operator.length = binaryString.length - returnVal.first.length
                     binaryString = returnVal.first
@@ -82,25 +78,31 @@ class Day16 {
                     val subPackageLength = binaryString.take(15).toInt(radix = 2)
                     binaryString = binaryString.drop(15)
                     operator.lengthTypeValue = subPackageLength
-                    val toParse = binaryString.take(subPackageLength)
+                    var toParse = binaryString.take(subPackageLength)
                     operator.binaryString = header + toParse
-                    val returnVal = parseString(toParse)
-                    operator.count = operator.count + returnVal.second.size
-                    operator.operators = operator.operators + returnVal.second
+
+                    while (toParse.isNotEmpty()) {
+                        val returnVal = parseString(toParse)
+                        operator.versions = operator.versions + returnVal.second.map { it.versions }.flatten()
+                        operator.operators = operator.operators + returnVal.second
+                        toParse = returnVal.first
+                    }
+
                     binaryString = binaryString.drop(subPackageLength)
                 }
                 val numbers = operator.operators.map { it.values }.flatten()
-                val newNum = when (operator.type) {
-                        0 -> numbers.sum()
-                        1 -> numbers.fold(1L) { acc, next -> next * acc }
-                        2 -> numbers.minOrNull()!!
-                        3 -> numbers.maxOrNull()!!
-                        5 -> if (numbers[0] > numbers[1]) 1 else 0
-                        6 -> if (numbers[0] < numbers[1]) 1 else 0
-                        7 -> if (numbers[0] == numbers[1]) 1 else 0
-                        else -> TODO()
-                    }
+                val newNum = when (operator.typeId) {
+                    0 -> numbers.sum()
+                    1 -> numbers.fold(1L) { acc, next -> next * acc }
+                    2 -> numbers.minOrNull()!!
+                    3 -> numbers.maxOrNull()!!
+                    5 -> if (numbers[0] > numbers[1]) 1 else 0
+                    6 -> if (numbers[0] < numbers[1]) 1 else 0
+                    7 -> if (numbers[0] == numbers[1]) 1 else 0
+                    else -> TODO()
+                }
                 operator.values.add(newNum)
+                return Pair(binaryString, operators)
 
             }
 
@@ -115,7 +117,7 @@ class Day16 {
         println("BinaryString: $binaryString")
         val (_, operators) = parseString(binaryString)
         println("operators $operators and sum: ${operators.size}")
-        return operators.sumOf { it.count }.toLong()
+        return operators.first().versions.sum().toLong()
     }
 
     fun part2(input: String, debug: Boolean = false): Long {
@@ -128,63 +130,8 @@ class Day16 {
         var (_, operators) = parseString(binaryString)
 
         println("operators")
-        operators.forEach { println("type: ${it.type}, version: ${it.version}, values: ${it.values}") }
-
-        // reduce it...
-        var ops = operators.map { it.copy() }
-
-        var reduceIndex = 0
-        while (reduceIndex < ops.size) {
-            val op = ops[reduceIndex]
-            if (op.lengthType == 1) {
-                val ops2 = ops.drop(reduceIndex + 1).take(op.lengthTypeValue)
-                op.values.addAll(ops2.map { it.values }.flatten())
-                ops = ops.filter { a -> !ops2.any { b -> a.id == b.id } }
-            }
-            reduceIndex++
-        }
-
-        println("ops")
-        ops.forEach { println(it) }
-
-
-//        var counter = 0
-//        while (counter) {
-//
-//        }
-
-
-        var lastOperator = operators.last()
-        while (operators.isNotEmpty()) {
-            lastOperator = operators.last()
-            val intNumbers = lastOperator.values
-            val newNum = when (lastOperator.type) {
-                4 -> intNumbers
-                else -> listOf(when (lastOperator.type) {
-                    0 -> intNumbers.sum()
-                    1 -> intNumbers.fold(1L) { acc, next -> next * acc }
-                    2 -> intNumbers.minOrNull()!!
-                    3 -> intNumbers.maxOrNull()!!
-                    5 -> if (intNumbers[0] > intNumbers[1]) 1 else 0
-                    6 -> if (intNumbers[0] < intNumbers[1]) 1 else 0
-                    7 -> if (intNumbers[0] == intNumbers[1]) 1 else 0
-                    else -> TODO()
-                })
-            }
-            if (operators.size > 1) {
-                operators = operators.dropLast(1).toMutableList()
-                operators.last().values.addAll(newNum)
-            } else {
-                operators = operators.dropLast(1).toMutableList()
-                lastOperator.values.clear()
-                lastOperator.values.addAll(newNum)
-            }
-
-        }
-
-        println("end")
-        operators.forEach { println(it) }
-        return lastOperator.values.first()
+        operators.forEach { println("type: ${it.typeId}, version: ${it.version}, values: ${it.values}") }
+        return operators.first().values.first()
     }
 
     fun main() {
